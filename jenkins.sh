6d5850429a6230d8994dd7c4201fa01f1e6f7120
Chinedu Olebu <74111891+golebu2020@users.noreply.github.com>
2023-06-09 18:47:34 -0700

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -41,3 +41,37 @@ npm -v
 
 echo "You're logged in as a root user"
 
+echo "Leave the shell mode"
+exit
+
+echo "Fetching the container id"
+jenkins_container_id=docker ps -a -q
+
+echo "Stop the container from running"
+
+docker stop $jenkins_container_id
+
+docker run -dp8080:8080 -p50000:50000 \
+-v jenkins_home:/var/jenkins_home \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v $(which docker):/usr/bin/docker  jenkins/jenkins:lts
+
+docker ps
+
+echo "log into the container as a root user..."
+docker exec -itu0 jenkins_container_id bash
+
+curl https://get.docker.com > dockerinstall && chmod 777 dockerinstall && ./dockerinstall
+
+echo "adding read-write permission for the file docker.sock"
+chmod 666 /var/run/docker.sock
+
+echo "logout and signin as a root user"
+docker exec -it jenkins_container_id bash
+
+echo "try to install redis container to test"
+docker pull redis
+
+echo "redis has been installed!"
+
+
