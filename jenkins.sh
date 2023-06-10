f6d2cbe017da449c48fcb97cbc5551a103e95a32
Chinedu Olebu <74111891+golebu2020@users.noreply.github.com>
2023-06-10 13:10:11 -0700

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,17 +4,18 @@ echo "Updating apt..."
 apt update
 
 echo "Installing docker and docker-compose..."
-apt install docker docker-compose
+apt install docker 
 
 echo "Installing jenkins docker image and running it..."
-docker run -dp8080:8080 -p50000:50000 \
+docker run --name jenkins_docker1 -d -p 8080:8080 -p50000:50000 \
 -v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
 
 echo "Fetching the container id"
-jenkins_container_id=docker ps -a -q
+jenkinscont1=docker ps -aqf jenkins_docker1
+
 
 echo "Log into the container as a root user"
-docker exec -it -u 0 $jenkins_container_id sh
+docker exec -u 0 -it $jenkinscont1 sh
 
 echo "Verify the Linux Distribution"
 cat /etc/issue
@@ -45,19 +46,20 @@ echo "Leave the shell mode"
 exit
 
 echo "Fetching the container id"
-jenkins_container_id=docker ps -a -q
+
 
 echo "Stop the container from running"
 
-docker stop $jenkins_container_id
+docker stop $jenkinscont1
 
-docker run -dp8080:8080 -p50000:50000 \
+docker run --name jenkins_docker2 -d -p 8080:8080 -p 50000:50000 \
 -v jenkins_home:/var/jenkins_home \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -v $(which docker):/usr/bin/docker jenkins/jenkins:lts
 
+jenkinscont2=docker ps -aqf jenkins_docker2
 echo "log into the container as a root user..."
-docker exec -it -u 0 $jenkins_container_id bash
+docker exec -it -u 0 $jenkinscont2 bash
 
 curl https://get.docker.com > dockerinstall && chmod 777 dockerinstall && ./dockerinstall
 
@@ -66,7 +68,7 @@ chmod 666 /var/run/docker.sock
 
 echo "logout and signin as a jenkins user"
 exit
-docker exec -it $jenkins_container_id bash
+docker exec -it $jenkinscont2 bash
 
 echo "try to install redis container to test"
 docker pull redis
