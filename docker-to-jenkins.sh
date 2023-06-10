57955b141950947be926f3dcad991a3351d5a59b
Chinedu Olebu <74111891+golebu2020@users.noreply.github.com>
2023-06-09 17:44:38 -0700

    Update docker-to-jenkins.sh

--- docker-to-jenkins.sh
+++ docker-to-jenkins.sh
@@ -1,1 +1,6 @@
+echo "stop the currently running container and attach a volume to jenkins"
+docker run -dp8080:8080 -p50000:50000 \
+-v jenkins_home:/var/jenkins_home \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v $(which docker):/usr/bin/docker  jenkins/jenkins:lts
 
