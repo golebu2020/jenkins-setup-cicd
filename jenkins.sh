043c9a3e176cb4ccc810f77049cad1d3d2a88668
Chinedu Olebu <74111891+golebu2020@users.noreply.github.com>
2023-06-09 17:29:47 -0700

    Create jenkins.sh
    Setting up jenkins

---
+++ jenkins.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+echo "Updating apt..."
+apt update
+
+echo "Installing docker and docker-compose..."
+apt install docker docker-compose
+
+echo "Installing jenkins docker image and running it..."
+docker run -dp8080:8080 -p50000:50000 \
+-v jenkins_home:/var/jenkins_home jenkins/jenkins:lts
+
+ehco "Fetching the container id"
+jenkins_container_id=docker ps -a -q
+
+echo "Log into the container as a root user"
+docker exec -u 0 -it $jenkins_container_id sh
+
+echo "Verify the Linux Distribution"
+cat /etc/issue
+
+echo "update apt"
+apt update
+
+echo "install curl..."
+apt install curl
+
+echo "download script for installing nodejs and npm"
+curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
+ls
+
+echo "Execute the script"
+bash nodesource_setup.sh
+
+echo "Install nodejs..."
+apt install nodejs
+
+nodejs -v
+npm -v
+
